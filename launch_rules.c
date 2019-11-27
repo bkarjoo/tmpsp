dfb764c2e328557f63d950566f467f56a8c0f323
bkarjoo <bkarjoo@gmail.com>
2019-11-27 12:16:45 -0500

    took out the launch rule periods

--- launch_rules.c
+++ launch_rules.c
@@ -6,9 +6,3 @@ launch_rules =
 {
   launch_filter
 }
-launch_rule_start_year =
-launch_rule_start_month =
-launch_rule_start_month_half =
-launch_rule_end_year =
-launch_rule_end_month =
-launch_rule_end_month_half =
