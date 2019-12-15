9757571f51c50858f8f9c8bed71ec12877ecf68a
Behrooz Karjoo <bkarjoo@gmail.com>
2019-12-15 14:14:10 -0500

    time options update

--- time_options.c
+++ time_options.c
@@ -1,5 +1,5 @@
 use_time_options = TRUE
-start_subscription = 9:21:00 AM
+start_subscription = 9:11:00 AM
 start_entering_positions = 9:30:00 AM
 stop_entering_positions = 3:57:00 PM
 cancel_all_pending_orders = 3:58:30 PM
